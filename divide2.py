2f61daf1c70c763fcc4d51d55e46fc2c0f823916
VuMinhNgoc <67154783+vuminhngoc22@users.noreply.github.com>
2021-10-15 22:55:19 +0700

    Add files via upload

---
+++ divide2.py
@@ -0,0 +1,30 @@
+import math
+def chiahaisolon(sobichia, sochia):
+
+	a = "";
+	b = 0;
+	temp = ord(sobichia[b]) - ord('0');
+	while (temp < sochia):
+		temp = (temp * 10 + ord(sobichia[b + 1]) - ord('0'));
+		b += 1;
+	
+	b += 1;
+
+	while ((len(sobichia)) > b):
+		
+		a += chr(math.floor(temp // sochia) + ord('0'));
+		
+		temp = ((temp % sochia) * 10 + ord(sobichia[b]) - ord('0'));
+		b += 1;
+
+	a += chr(math.floor(temp // sochia) + ord('0'));
+	
+
+	if (len(a) == 0):
+		return "0";
+	
+	return a;
+
+sobichia = "1248163264128256512";
+sochia = 125;
+print(chiahaisolon(sobichia, sochia));
